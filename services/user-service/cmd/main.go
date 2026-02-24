1ef2b38d485a4291aeb88dc125908a5f1489c11d
peterouob <hellolinpeter@gmail.com>
2026-02-24 18:02:05 +0800

    fix: grpc server timeout and make sure grpc server starts first

--- services/user-service/cmd/main.go
+++ services/user-service/cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"log"
 	"net"
 	"net/http"
 	"os"
@@ -17,44 +16,58 @@ import (
 	"github.com/peterouob/seckill_service/services/user-service/internal/router"
 	"github.com/peterouob/seckill_service/services/user-service/internal/service"
 	"github.com/peterouob/seckill_service/utils/database"
+	"github.com/peterouob/seckill_service/utils/logs"
+	"github.com/peterouob/seckill_service/utils/pool"
 	"google.golang.org/grpc"
 )
 
 func main() {
-
+	logs.InitLogger("user")
 	db := database.ConnPostgresql()
 	userRepo := repository.NewUserRepo(db)
 	userService := service.NewUserService(userRepo)
-	userController := controller.NewUserController(userService)
-
-	r := router.InitRouter(userController)
-
 	userGrpc := usergrpc.NewUserGrpcHandlers(userService)
-	go func() {
-		lis, err := net.Listen("tcp", ":50051")
-		if err != nil {
-			log.Fatalf("Grpc Listen Error: %v", err)
-		}
 
+	grpcChannel := make(chan struct{})
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+	go func() {
+		lis, err := net.Listen("tcp", ":50050")
+		if err != nil {
+			logs.Error("failed to listen: %v\n", err)
+		}
 		grpcServer := grpc.NewServer()
 		userproto.RegisterUserServiceServer(grpcServer, userGrpc)
-
-		log.Println("gRPC server start on :50051")
+		logs.Log("gRPC server start on :50050")
+		close(grpcChannel)
 		if err := grpcServer.Serve(lis); err != nil {
-			log.Fatalf("gRPC 伺服器啟動失敗: %v", err)
+			logs.Error("start grpc server error", err)
 		}
 	}()
 
+	select {
+	case <-grpcChannel:
+		logs.Log("grpc server ready ...")
+	case <-ctx.Done():
+		logs.Warn("grpc server start timeout")
+
+	}
+
+	p := pool.New("127.0.0.1:50050", pool.DefaultOption)
+	conn, _ := p.Get()
+	client := userproto.NewUserServiceClient(conn.Value())
+	userController := controller.NewUserController(client)
+
+	r := router.InitRouter(userController)
+
 	server := &http.Server{
-		Addr:    ":8081",
+		Addr:    ":8083",
 		Handler: r,
 	}
 
 	serverErrors := make(chan error, 1)
 	go func() {
-		database.ConnRedis()
-		database.ConnPostgresql()
-		log.Println("Starting server ...")
+		logs.Log("Starting server ...")
 		serverErrors <- server.ListenAndServe()
 	}()
 
@@ -62,14 +75,14 @@ func main() {
 	signal.Notify(shutDown, os.Interrupt, syscall.SIGTERM)
 	select {
 	case err := <-serverErrors:
-		log.Printf("Error starting server ... %v\n", err)
+		logs.Logf("Error starting server ... %v\n", err)
 	case sig := <-shutDown:
-		log.Printf("Server is shutting due to the %v signal\n", sig)
+		logs.ErrorMsgF("Server is shutting due to the %v signal\n", sig)
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
 		if err := server.Shutdown(ctx); err != nil {
-			log.Printf("Could n ot stdio the server gracefully %v\n", err)
+			logs.ErrorMsgF("Could n ot stdio the server gracefully %v\n", err)
 			_ = server.Close()
 		}
 	}
